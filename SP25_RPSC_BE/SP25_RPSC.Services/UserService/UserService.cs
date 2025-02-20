04b1851ee4b03a2bb7414b46344a00fd813ed105
NguyenNhatHao <nhathao230803@gmail.com>
2025-02-20 21:28:28 +0700

    [feat] Get customer + landlord

---
+++ SP25_RPSC_BE/SP25_RPSC.Services/UserService/UserService.cs
@@ -0,0 +1,34 @@
+﻿using SP25_RPSC.Data.Entities;
+using SP25_RPSC.Data.Models.UserModels.Response;
+using SP25_RPSC.Data.Repositories.UserRepository;
+using SP25_RPSC.Data.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP25_RPSC.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<ListCustomerRes>> GetAllCustomer()
+        {
+            var res = await _unitOfWork.CustomerRepository.GetAllCustomer();
+                return res;
+        }
+
+        public async Task<IEnumerable<ListLandlordRes>> GetAllLandLord()
+        {
+           var res = await _unitOfWork.LandlordRepository.GetAllLanlord();
+            return res;
+        }
+    }
+}
