1cfa6ff13138fb292c86f493c72f4f7746c971f8
NguyenNhatHao <nhathao230803@gmail.com>
2025-04-02 21:33:27 +0700

    [fea]Create_FeedBack

---
+++ SP25_RPSC_BE/SP25_RPSC.Data/Models/FeedbackModel/Request/FeedBackCustomerRequestModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP25_RPSC.Data.Models.FeedbackModel.Request
+{
+    public class FeedBackCustomerRequestModel
+    {
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Status { get; set; }
+
+        public string RevieweeId { get; set; }
+
+        public required List<IFormFile> Images { get; set; }
+    }
+}
