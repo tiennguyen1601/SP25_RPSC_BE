02ddc7ed1649d29c2b4009b1753087d0b067067d
tien nguyen vi <nguyevitien2003@gmail.com>
2025-04-13 21:32:25 +0700

    [Feat] add docker

--- SP25_RPSC_BE/SP25_RPSC.Controllers/Program.cs
+++ SP25_RPSC_BE/SP25_RPSC.Controllers/Program.cs
@@ -189,11 +189,11 @@ builder.Services.AddControllers()
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
