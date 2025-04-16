0d6743f994049b222db6c118ab49aaf55cc64e3a
tien nguyen vi <nguyevitien2003@gmail.com>
2025-04-16 14:48:15 +0700

    [Feat] view profile and update profile

---
+++ SP25_RPSC_BE/SP25_RPSC.Data/Models/UserModels/Request/UpdateUserRequestModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP25_RPSC.Data.Models.UserModels.Request
+{
+    public class UpdateUserRequestModel
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+        public DateOnly? Dob { get; set; }
+        public string Avatar { get; set; }
+    }
+
+    public class UpdateCustomerRequestModel
+    {
+        public string Preferences { get; set; }
+        public string LifeStyle { get; set; }
+        public string BudgetRange { get; set; }
+        public string PreferredLocation { get; set; }
+        public string Requirement { get; set; }
+        public string CustomerType { get; set; }
+    }
+
+}
